c93b5aa5c618ceddbf1a36bf9ffc06b44e98b372
aluminumbulb <47339849+aluminumbulb@users.noreply.github.com>
2021-11-16 12:32:40 -0800

    Removed Monobehavior heritage from BTPriorityQueue

--- Cmpm146 Final/Assets/Scripts/BehaviorTree/BTPriorityQueue.cs
+++ Cmpm146 Final/Assets/Scripts/BehaviorTree/BTPriorityQueue.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BTPriorityQueue : MonoBehaviour
+public class BTPriorityQueue
 {
     List<BTNode> pq;
     
