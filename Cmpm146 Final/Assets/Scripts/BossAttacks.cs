9402750eeb0d9b86d42f05ef404f179d699d7367
Astric215 <davidiwd215@gmail.com>
2021-11-18 21:11:28 -0800

    Made it easier to change and visualize attack ranges

--- Cmpm146 Final/Assets/Scripts/BossAttacks.cs
+++ Cmpm146 Final/Assets/Scripts/BossAttacks.cs
@@ -9,6 +9,11 @@ public class BossAttacks : MonoBehaviour
     public Transform centerPoint;
     public LayerMask enemyLayers;
     public int beamLength = 8;
+    public int aoeSize = 1;
+    public int swipeSize = 2;
+    public bool showSwipe = true;
+    public bool showBeams = true;
+    public bool showAOE = true;
     // Update is called once per frame
     void Update()
     {
@@ -37,7 +42,7 @@ public class BossAttacks : MonoBehaviour
         //Play animation 
 
         //collect the objects hit
-        Collider2D[] hitHero = Physics2D.OverlapBoxAll(swipeRightPoint.position, new Vector2(2, 3), 0,enemyLayers);
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(swipeRightPoint.position, new Vector2(swipeSize, swipeSize+1), 0,enemyLayers);
         
         //send meesage to Hero/ hit the Hero
         foreach (Collider2D Hero in hitHero)
@@ -51,7 +56,7 @@ public class BossAttacks : MonoBehaviour
         //Play animation 
 
         //collect the objects hit
-        Collider2D[] hitHero = Physics2D.OverlapBoxAll(swipeLeftPoint.position, new Vector2(2, 3), 0, enemyLayers);
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(swipeLeftPoint.position, new Vector2(swipeSize, swipeSize+1), 0, enemyLayers);
 
         //send meesage to Hero/ hit the Hero
         foreach (Collider2D Hero in hitHero)
@@ -65,7 +70,7 @@ public class BossAttacks : MonoBehaviour
         //Play animation 
 
         //collect the objects hit
-        Collider2D[] hitHero = Physics2D.OverlapBoxAll(centerPoint.position, new Vector2(3, 3), 0, enemyLayers);
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(centerPoint.position, new Vector2(aoeSize, aoeSize), 0, enemyLayers);
 
         //send meesage to Hero/ hit the Hero
         foreach (Collider2D Hero in hitHero)
@@ -98,24 +103,28 @@ public class BossAttacks : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        if(swipeRightPoint != null)
+        if(swipeRightPoint != null && showSwipe)
         {
             
-            Gizmos.DrawWireCube(swipeRightPoint.position, new Vector3(2, 3, 1));
+            Gizmos.DrawWireCube(swipeRightPoint.position, new Vector3(swipeSize+1, swipeSize+2, 1));
         }
 
-        if (swipeLeftPoint != null)
+        if (swipeLeftPoint != null && showSwipe)
         {
-            Gizmos.DrawWireCube(swipeLeftPoint.position, new Vector3(2, 3, 1));
+            Gizmos.DrawWireCube(swipeLeftPoint.position, new Vector3(swipeSize+1, swipeSize+2, 1));
         }
-        
-        if (centerPoint != null)
+
+        if (centerPoint != null && showAOE)
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawWireCube(centerPoint.position, new Vector3(4, 4, 1));
+            Gizmos.DrawWireCube(centerPoint.position, new Vector3(aoeSize + 1, aoeSize + 1, 1));
+        }
+
+        if (centerPoint != null && showBeams)
+        {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireCube(centerPoint.position, new Vector3(1, beamLength, 1));
-            Gizmos.DrawWireCube(centerPoint.position, new Vector3(beamLength, 1, 1));
+            Gizmos.DrawWireCube(centerPoint.position, new Vector3(2, beamLength, 1));
+            Gizmos.DrawWireCube(centerPoint.position, new Vector3(beamLength, 2, 1));
         }
         
 
