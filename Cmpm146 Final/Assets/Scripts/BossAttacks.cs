8e8da8ee4daa8522fb3c1b5048bb331907caac36
Astric215 <davidiwd215@gmail.com>
2021-11-18 19:42:54 -0800

    Added boss attacks
    Left Swipe: X
    Right Swipe: Z
    Beams: C
    AOE: space
    no naimations rn and all it does is print to debug logs

---
+++ Cmpm146 Final/Assets/Scripts/BossAttacks.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttacks : MonoBehaviour
+{
+    public Transform swipeRightPoint;
+    public Transform swipeLeftPoint;
+    public Transform centerPoint;
+    public LayerMask enemyLayers;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SwipeRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SwipeLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Beams();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AOE();
+        }
+    }
+    void SwipeRight()
+    {
+        //Play animation 
+
+        //collect the objects hit
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(swipeRightPoint.position, new Vector2(2, 3), 0,enemyLayers);
+        
+        //send meesage to Hero/ hit the Hero
+        foreach (Collider2D Hero in hitHero)
+        {
+            Debug.Log("hit " + Hero.name + " with Swipe Right");
+        }
+    }
+
+    void SwipeLeft()
+    {
+        //Play animation 
+
+        //collect the objects hit
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(swipeLeftPoint.position, new Vector2(2, 3), 0, enemyLayers);
+
+        //send meesage to Hero/ hit the Hero
+        foreach (Collider2D Hero in hitHero)
+        {
+            Debug.Log("hit " + Hero.name + " with Swipe Left");
+        }
+    }
+
+    void AOE()
+    {
+        //Play animation 
+
+        //collect the objects hit
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(centerPoint.position, new Vector2(3, 3), 0, enemyLayers);
+
+        //send meesage to Hero/ hit the Hero
+        foreach (Collider2D Hero in hitHero)
+        {
+            Debug.Log("hit " + Hero.name + " with AOE");
+        }
+    }
+
+    void Beams()
+    {
+        //Play animation 
+
+        //collect the objects hit
+        Collider2D[] hitHero = Physics2D.OverlapBoxAll(centerPoint.position, new Vector2(8, 1), 0, enemyLayers);
+        //send meesage to Hero/ hit the Hero
+        foreach (Collider2D Hero in hitHero)
+        {
+            Debug.Log("hit " + Hero.name + " with Beams horizontal");
+        }
+
+        hitHero = Physics2D.OverlapBoxAll(centerPoint.position, new Vector2(1, 8), 0, enemyLayers);
+
+        foreach (Collider2D Hero in hitHero)
+        {
+            Debug.Log("hit " + Hero.name + " with Beams vertical");
+        }
+    }
+
+    //This is called to draw attack boxes when the boss is selected
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        if(swipeRightPoint != null)
+        {
+            
+            Gizmos.DrawWireCube(swipeRightPoint.position, new Vector3(2, 3, 1));
+        }
+
+        if (swipeLeftPoint != null)
+        {
+            Gizmos.DrawWireCube(swipeLeftPoint.position, new Vector3(2, 3, 1));
+        }
+        
+        if (centerPoint != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(centerPoint.position, new Vector3(4, 4, 1));
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(centerPoint.position, new Vector3(1, 8, 1));
+            Gizmos.DrawWireCube(centerPoint.position, new Vector3(8, 1, 1));
+        }
+        
+
+    }
+}
